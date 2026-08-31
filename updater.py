6e714f47714bf536f37a452f0fd3760a33a708a8
Ahmad Ansari <ahmadansari967509@gmail.com>
2026-08-31 17:10:03 +0530

    Change Firestore collection name to 'test_notices'
    Updated Firestore collection name from 'live_notices' to 'test_notices'.

--- updater.py
+++ updater.py
@@ -45,7 +45,7 @@ if not os.path.exists(FIREBASE_SERVICE_ACCOUNT_JSON):
 cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT_JSON)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-firestore_collection = db.collection("live_notices")
+firestore_collection = db.collection("test_notices")
 
 r2_client = boto3.client(
     service_name='s3',
